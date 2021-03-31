92fce5548bc58002513395527e246d0eac7dab87
Osheen Pandita <75720970+osheenpandita@users.noreply.github.com>
2021-03-31 14:03:46 +0530

    Write a C program to perform division

---
+++ lab02.c (D).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    float number1, number2, division;
+
+    printf("Enter the first number:\n");
+    scanf("%f", &number1);
+
+    printf("Enter the second number:\n");
+    scanf("%f", &number2);
+
+    division = number1 / number2;
+    printf("%f", division);
+
+    return 0;
+}
